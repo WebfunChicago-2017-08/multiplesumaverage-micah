69d08283cc9aeda844f7f1d44e283984da558d58
JMicahH <micah.haid@gmail.com>
2017-09-06 12:58:10 -0500

    Multiples Sum Average Exercise in Python

---
+++ multiplesumsavg.py
@@ -0,0 +1,36 @@
+"""
+Mutliples 1____________
+odds = 1
+while odds < 1000:
+    print odds
+    odds += 2"""
+
+
+"""
+Mutliples 2: Method 1____________
+for count in range (5, 1000000, 3):
+    print count"""
+
+"""
+Mutliples 2: Method 2____________
+multiples = 5    
+while multiples < 1000000:
+    print multiples
+    multiples += 5
+"""
+
+"""Sum List____________
+a = [1, 2, 5, 10, 255, 3]
+sum = 0
+for count in a:
+    sum += count
+print sum    """
+
+"""Average List___________
+"""
+a = [1, 2, 5, 10, 255, 3, 4]
+sum = 0
+for count in a:
+    sum += count
+avg = sum/len(a)
+print avg
